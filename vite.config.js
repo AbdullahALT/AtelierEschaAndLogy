202b42808a8e04ea400a53419225e20dc270c4ab
Abdullah Altamimi <ab.altamimi@tamkeentech.sa>
2025-05-09 11:39:56 +0300

    prepare github page deployment

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
+import ghPages from 'vite-plugin-gh-pages';
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), ghPages()],
+  base: 'AtelierEschaAndLogy',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src')
